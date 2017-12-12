8e6eb42d0d5347687c7a74d5f1a760ee7ecd4d92
execter@gmail.com <Akaradech Sutheesuntornkul>
2017-12-12 16:02:14 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default () => 
+<div>
+    <Head>
+        <title>Welcome Next.js</title>
+        <meta charSet="utf-8" />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+    Welcome Next.js V4
+</div>
